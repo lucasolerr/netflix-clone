3b63541fc72e0965c05401897b95ff7977895864
ECELucaSoler <92515171+ECELucaSoler@users.noreply.github.com>
2023-03-26 17:33:33 +0200

    final

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{jx,ts,jsx,tsx}",
+    "./pages/**/*.{jx,ts,jsx,tsx}",
+    "./components/**/*.{jx,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
